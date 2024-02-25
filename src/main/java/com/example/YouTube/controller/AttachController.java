3c71444f107d60a60b3a9758fba9051073e29867
husniddin100 <uchanal10@gmail.com>
2024-02-25 12:07:58 +0500

    attach finish

--- src/main/java/com/example/YouTube/controller/AttachController.java
+++ src/main/java/com/example/YouTube/controller/AttachController.java
@@ -2,8 +2,10 @@ package com.example.YouTube.controller;
 
 import com.example.YouTube.dto.AttachDTO;
 import com.example.YouTube.service.AttachService;
+import com.example.YouTube.util.SpringSecurityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,22 +18,39 @@ import org.springframework.web.multipart.MultipartFile;
 public class AttachController {
     @Autowired
     private AttachService attachService;
+
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping(value = "/upload")
     public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file) {
         AttachDTO fileName = attachService.save(file);
         return ResponseEntity.ok().body(fileName);
     }
+
     @GetMapping(value = "/open/{fileName}", produces = MediaType.ALL_VALUE)
     public byte[] open(@PathVariable("fileName") String fileName) {
         return attachService.open(fileName);
     }
 
-    @GetMapping("/public/download/{fineName}")
+    @GetMapping("/download/{fineName}")
     public ResponseEntity<Resource> download(@PathVariable("fineName") String fileName) {
         Resource file = attachService.download(fileName);
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
                 "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/get")
+    public ResponseEntity<?> getWithPage(@RequestParam("page") Integer page, @RequestParam("size") Integer size) {
+        Page<AttachDTO> result = attachService.getWithPage(page, size);
+        return ResponseEntity.ok(result);
+    }
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<?> deleteById(@PathVariable("fileName") String fileName) {
+        String result = attachService.deleteById(fileName);
+        return ResponseEntity.ok(result);
+    }
+
 
 }
